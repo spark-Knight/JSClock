e56e1bdf8ff4d1296ada4e20d4d757c033c11cb2
spark-Knight <67824440+spark-Knight@users.noreply.github.com>
2022-02-16 17:10:09 +0530

    update code

--- JSclock.js
+++ JSclock.js
@@ -1,14 +1,17 @@
-let Days = ["", "Monday", "Tuesday", "Wednesday", "Thursday", "Friday", "Saturday", "Sunday"];
+
+const Days = ["hi", "Monday", "Tuesday", "Wednesday", "Thursday", "Friday", "Saturday", "Sunday"];
 
 function dayOfWeek() {
     let myDate = new Date();
     TodayDay = myDate.getDay();
     return Days[TodayDay];
 }
-
-a = new Date();
-let ToDate = a.toLocaleDateString();
-let Time = a.getHours() + " : " + a.getMinutes() + " : " + a.getSeconds();
-let Day = dayOfWeek();
-document.getElementById("time").className = Time + Day;
-
+setInterval(() => {
+    a = new Date();
+    let ToDate = a.toLocaleDateString();
+    let Time = a.getHours() + ":" + a.getMinutes() + ":" + a.getSeconds();
+    let Day = dayOfWeek();
+    document.getElementById("time").innerHTML = Time;
+    document.getElementById("Date").innerHTML = ToDate;
+    document.getElementById("Day").innerHTML = Day;
+},1000);
